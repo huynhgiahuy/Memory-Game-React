08608760b1685341bfa5358c03f6b043cb703d7b
Huỳnh Gia Huy <giahuy009009@gmail.com>
2022-02-10 13:15:00 +0700

    Init Project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Cards from "./components/Cards"
+function App() {
+  return(
+    <div className='App'>
+      <h1>Memory Game with ReactJS</h1>
+      <Cards/>
+    </div>
+  )
+}
+
+export default App
